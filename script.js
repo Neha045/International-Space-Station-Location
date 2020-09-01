1358b1aa62351c291153acb13c29f5dfc4cc1868
Neha Sonpure <sonpureneha@gmail.com>
2020-09-01 19:02:37 +0530

    Add Particles

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 200, // Number of particles
+        "density": {
+          "enable": true,
+          "value_area": 789 // Density is bigger with smaller number
+        }
+      },
+      "color": { // Color only for nodes
+        "value": ["#fff", "#600277" ]  // You can add one more more colors
+      },
+      "opacity": { // Node opacity
+        "value": 0.5, 
+        "random": false,
+        "anim": { // Node opacity animation, with speed and min opacity
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": { // Node size
+        "value": 1,
+        "random": true,
+        "anim": { // Node size animation, with speed and min size
+          "enable": true,
+          "speed": 8,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": { // Lines between nodes
+        "enable": true,
+        "distance": 100, // Distance between node when line will be added
+        "color": "#8940ff",
+        "opacity": 0.3,
+        "width": 2
+      },
+      "move": { // Node movementm with speed, directions..
+        "enable": true,
+        "speed": 1.8,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      },
+    }
+  });
 const issMap = L.map('issmap').setView([0, 0], 2);
 // Adding custom icon
 const issIcon = L.icon({
